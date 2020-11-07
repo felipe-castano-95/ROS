2be999cbe21d4bbacd314acfb967f3f197aae634
root <lcastl02@estudiantes.unileon.es>
2020-11-07 21:50:12 +0000

    Initial commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /headless/ros_ws/src
+# Build directory: /headless/ros_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(harderingros)
+subdirs(my_first_package)
---
+++ build/harderingros/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /headless/ros_ws/src/harderingros
+# Build directory: /headless/ros_ws/build/harderingros
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
